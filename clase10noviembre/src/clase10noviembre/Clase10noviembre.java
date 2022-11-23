dcd4f249a81c451f8f88c2ff1adff28d94442729
The Vampi <luisitorespawn@gmail.com>
2022-11-23 00:05:58 -0600

    Agregados metodos adicionales de arreglos

--- clase10noviembre/src/clase10noviembre/Clase10noviembre.java
+++ clase10noviembre/src/clase10noviembre/Clase10noviembre.java
@@ -4,7 +4,57 @@ import javax.swing.*;
 
 public class Clase10noviembre {
 
+    /*⠄⠄⠄⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄
+⠄⠄⠄⣾⣿⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄⠸⢀⣿⠄
+⠄⠄⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄
+⠄⠄⢸⣇⠻⣿⣿⣿⣧⣀⢀⣠⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⠄
+⠄⢀⢸⣿⣷⣤⣤⣤⣬⣙⣛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡍⠄⠄⢀⣤⣄⠉⠋⣰
+⠄⣼⣖⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇⢀⣤
+⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴⣿⡗
+⢀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄
+⢸⣿⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄
+⠘⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾⠃⠄
+⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃⠄⠄
+⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⠄⣿⣿⡀⣾⣿⣿⣿⣿⣛⠛⠁⠄⠄⠄
+⠄⠄⠄⠄⠈⠛⢿⣿⣿⣿⠁⠞⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠉⠻⣿⣿⣾⣦⡙⠻⣷⣾⣿⠃⠿⠋⠁⠄⠄⠄⠄⠄⢀⣠⣴
+⣿⣿⣿⣶⣶⣮⣥⣒⠲⢮⣝⡿⣿⣿⡆⣿⡿⠃⠄⠄⠄⠄⠄⠄⠄⣠⣴⣿⣿⣿
+   LIBRERIAS DE MATRICES uwu
+⠀⠀⠘⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀
+⠀⠀⠀⠑⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡔⠁⠀⠀⠀
+⠀⠀⠀⠀⠈⠢⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠴⠊⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⢀⣀⣀⣀⣀⣀⡀⠤⠄⠒⠈⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠘⣀⠄⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀
+⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠛⠛⠋⠉⠈⠉⠉⠉⠉⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣿⣿
+⣿⣿⣿⣿⡏⣀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿
+⣿⣿⣿⢏⣴⣿⣷⠀⠀⠀⠀⠀⢾⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿
+⣿⣿⣟⣾⣿⡟⠁⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣷⢢⠀⠀⠀⠀⠀⠀⠀⢸⣿
+⣿⣿⣿⣿⣟⠀⡴⠄⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⣿
+⣿⣿⣿⠟⠻⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠶⢴⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⣿
+⣿⣁⡀⠀⠀⢰⢠⣦⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⡄⠀⣴⣶⣿⡄⣿
+⣿⡋⠀⠀⠀⠎⢸⣿⡆⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⠗⢘⣿⣟⠛⠿⣼
+⣿⣿⠋⢀⡌⢰⣿⡿⢿⡀⠀⠀⠀⠀⠀⠙⠿⣿⣿⣿⣿⣿⡇⠀⢸⣿⣿⣧⢀⣼
+⣿⣿⣷⢻⠄⠘⠛⠋⠛⠃⠀⠀⠀⠀⠀⢿⣧⠈⠉⠙⠛⠋⠀⠀⠀⣿⣿⣿⣿⣿
+⣿⣿⣧⠀⠈⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⠀⠀⠀⠀⢀⢃⠀⠀⢸⣿⣿⣿⣿
+⣿⣿⡿⠀⠴⢗⣠⣤⣴⡶⠶⠖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡸⠀⣿⣿⣿⣿
+⣿⣿⣿⡀⢠⣾⣿⠏⠀⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠉⠀⣿⣿⣿⣿
+⣿⣿⣿⣧⠈⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿
+⣿⣿⣿⣿⡄⠈⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣦⣄⣀⣀⣀⣀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠙⣿⣿⡟⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠁⠀⠀⠹⣿⠃⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⢐⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⠿⠛⠉⠉⠁⠀⢻⣿⡇⠀⠀⠀⠀⠀⠀⢀⠈⣿⣿⡿⠉⠛⠛⠛⠉⠉
+⣿⡿⠋⠁⠀⠀⢀⣀⣠⡴⣸⣿⣇⡄⠀⠀⠀⠀⢀⡿⠄⠙⠛⠀⣀⣠⣤⣤⠄⠀      
+     */
     public static void main(String[] args) {
+
+        //AQUI VAMOS A PROBAR CADA FUNCION UWU
         Matrices objeto1 = new Matrices();
         JOptionPane.showMessageDialog(null, "Bienvenido a la calculadora de matrices, vamos a crear la primer matriz");
         //Creando y leyendo la primera matriz
@@ -28,16 +78,37 @@ public class Clase10noviembre {
         //Calculando la RESTA de la SEGUNDA menos la PRIMERA
         int[][] matriz5 = new int[matriz1.length][matriz1[0].length];
         objeto1.restarMatriz(matriz2, matriz1, matriz5);
-         
+
         //Creando una matriz para rellenar con multiplos de 3
         int[][] matrizMultiplosDe3;
         matrizMultiplosDe3 = objeto1.asignarDimensionesAArreglo();
         matrizMultiplosDe3 = objeto1.llenarMatrizEsquinaSuperiorDerecha(matrizMultiplosDe3);
 
-        
         //Mostrando los resultados finales
         JOptionPane.showMessageDialog(null, "Matriz 1\n" + objeto1.mostrarMatriz(matriz1) + "\n\nMatriz 2:\n" + objeto1.mostrarMatriz(matriz2) + "\n\nMatriz 1 + Matriz 2:\n" + objeto1.mostrarMatriz(matriz3) + "\n\nMatriz 1 - Matriz 2:\n" + objeto1.mostrarMatriz(matriz4) + "\n\nMatriz 2 - Matriz 1:\n" + objeto1.mostrarMatriz(matriz5) + "\n\nMatriz llenada con multiplos de 3:\n" + objeto1.mostrarMatriz(matrizMultiplosDe3));
-        
+
+        //Sumando las diagonales normal e invertida una matriz cuadrada
+        int[][] matrizDiagonal;
+        matrizDiagonal = objeto1.leerMatriz();
+        JOptionPane.showMessageDialog(null, objeto1.mostrarMatriz(matrizDiagonal));
+        objeto1.obtenerSumaDiagonalMatrizCuadrada(matrizDiagonal);
+
+        //Ordenando un arreglo de tipo ENTERO CON METODO BURBUJA
+        objeto1.ordenamientoMetodoBurbuja();
+
+        //Ordenando un arreglo de 2 dimensiones de MENOR A MAYOR UTILIZANDO METODO DE LA BURBUJA
+        int[][] arregloParaOrdenar;
+        arregloParaOrdenar = objeto1.leerMatriz();
+        JOptionPane.showMessageDialog(null, objeto1.mostrarMatriz(arregloParaOrdenar));
+        objeto1.arregloDosDimensionesOrdenado(arregloParaOrdenar);
+        JOptionPane.showMessageDialog(null, "Arreglo ordenado de menor a mayor: \n" + objeto1.mostrarMatriz(arregloParaOrdenar));
+
+        //Obteniendo una matriz transpuesta de dimensiones n*n
+        int[][] arregloParaTransponer;
+        arregloParaTransponer = objeto1.leerMatriz();
+        JOptionPane.showMessageDialog(null, objeto1.mostrarMatriz(arregloParaTransponer));
+        arregloParaTransponer = objeto1.obtenerMatrizTranspuesta(arregloParaTransponer);
+        JOptionPane.showMessageDialog(null, "RESULTADO MATRIZ TRANSPUESTA:\n" + objeto1.mostrarMatriz(arregloParaTransponer));
     }
 
 }
@@ -67,7 +138,7 @@ class Matrices {
         //Llenando con datos
         for (int filaParaLlenar = 0; filaParaLlenar < unArreglo.length; filaParaLlenar++) {
             for (int columnaParaLlenar = 0; columnaParaLlenar < unArreglo[0].length; columnaParaLlenar++) {
-                unArreglo[filaParaLlenar][columnaParaLlenar] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el valor de la fila " + filaParaLlenar + " columna " + columnaParaLlenar));
+                unArreglo[filaParaLlenar][columnaParaLlenar] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el valor de la fila " + (filaParaLlenar + 1) + " columna " + (columnaParaLlenar + 1)));
             }
         }
         return (unArreglo);
@@ -117,6 +188,21 @@ class Matrices {
 
     }
 
+    //Metodo para obtener una matriz transpuesta (Los renglones pasan a columnas y las columnas a renglones)
+    int[][] obtenerMatrizTranspuesta(int[][] matrizAConvertir) {
+
+        //Creando la nueva matriz con dimensiones invertidas de la original
+        int[][] matrizTranspuesta = new int[matrizAConvertir[0].length][matrizAConvertir.length];
+
+        //Asignando los nuevos valores a la nueva matriz
+        for (int filaDeLaTranspuesta = 0; filaDeLaTranspuesta < matrizAConvertir[0].length; filaDeLaTranspuesta++) {
+            for (int columnaDeLaTranspuesta = 0; columnaDeLaTranspuesta < matrizAConvertir.length; columnaDeLaTranspuesta++) {
+                matrizTranspuesta[filaDeLaTranspuesta][columnaDeLaTranspuesta] = matrizAConvertir[columnaDeLaTranspuesta][filaDeLaTranspuesta];
+            }
+        }
+        return (matrizTranspuesta);
+    }
+
     //Metodo para rellenar matrices con multiplos de 3 (EMPEZANDO ESQUINA SUPERIOR DERECHA)en esta forma: ← ↓ → ↓ ← ...
     int[][] llenarMatrizEsquinaSuperiorDerecha(int[][] matrizAEvaluar) {
         JOptionPane.showMessageDialog(null, "Vamos a rellenar una matriz con multiplos de 3");
@@ -142,4 +228,126 @@ class Matrices {
         }
         return (unArreglo);
     }
+
+    //Metodo para sumar en diagonal normal y diagonal invertida una MATRIZ CUADRADA
+    void obtenerSumaDiagonalMatrizCuadrada(int[][] matrizParaSacarDiagonal) {
+        int sumatoriaDiagonalPrincipal = 0, sumatoriaDiagonalInvertida = 0;
+        String aux;
+        if (matrizParaSacarDiagonal.length == matrizParaSacarDiagonal[0].length) {
+            //Sacando la sumatoria de la DIAGONAL PRINCIPAL ↘
+            for (int renglon = 0; renglon < matrizParaSacarDiagonal.length; renglon++) {
+                sumatoriaDiagonalPrincipal = sumatoriaDiagonalPrincipal + matrizParaSacarDiagonal[renglon][renglon];
+            }
+            //Sacando la sumatoria de la DIAGONAL INVERTIDA ↙
+            for (int fila = 0, columna = matrizParaSacarDiagonal[0].length - 1; columna >= 0; fila++, columna--) {
+                sumatoriaDiagonalInvertida = sumatoriaDiagonalInvertida + matrizParaSacarDiagonal[fila][columna];
+            }
+            aux = "La sumatoria de la diagonal PRINCIPAL es: " + sumatoriaDiagonalPrincipal + "\nLa sumatoria de la diagonal INVERTIDA es: " + sumatoriaDiagonalInvertida;
+            JOptionPane.showMessageDialog(null, aux);
+        } else {
+            JOptionPane.showMessageDialog(null, "No se puede realizar la operacion, no es una matriz cuadrada");
+        }
+
+    }
+
+    //METODO BURBUJA para ordenar arreglos de UNA DIMENSION (DE TIPO INT)
+    void ordenamientoMetodoBurbuja() {
+
+        int variableAuxiliar;
+
+        //Asignando la cantidad 
+        int filas = Integer.parseInt(JOptionPane.showInputDialog("Teclea numero de elementos para el arreglo"));
+
+        //Creacion del arreglo
+        int[] arreglo = new int[filas];
+        //Asignando valores
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un valor entero" + (i + 1)));
+        }
+
+        //Ordenandolos elementos
+        for (int pasadas = 1; pasadas < arreglo.length; pasadas++) {
+            for (int comparaciones = 1; comparaciones <= arreglo.length - 1; comparaciones++) {
+
+                if (arreglo[comparaciones - 1] > arreglo[comparaciones]) {
+                    variableAuxiliar = arreglo[comparaciones - 1];
+                    arreglo[comparaciones - 1] = arreglo[comparaciones];
+                    arreglo[comparaciones] = variableAuxiliar;
+                }
+
+                //LO SIGUIENTE ES PARA VERIFICAR EN CONSOLA COMO FUNCIONA EL ALGORITMO EN LAS COMPARACIONES
+                //La siguiente linea es solo para verificar en consola
+                System.out.println("Comparacion Actual:" + comparaciones);
+
+                //Mostrando los resultados ordenados del arreglo
+                for (int i = 0; i < arreglo.length; i++) {
+                    System.out.println(arreglo[i]);
+                }
+            }//AQUI TERMINA CICLO DE COMPARACIONES
+
+            //LO SIGUIENTE ES PARA VERIFICAR EN CONSOLA COMO FUNCIONA EL ALGORITMO EN LAS PASADAS
+            //La siguiente linea es solo para verificar en consola
+            System.out.println("Pasada actual:" + pasadas);
+
+            //Mostrando los resultados ordenados del arreglo EN CONSOLA
+            for (int i = 0; i < arreglo.length; i++) {
+                System.out.println(arreglo[i]);
+            }
+        }//AQUI TERMINA CICLO DE PASADAS
+
+        //Mostrando los resultados FINALES ordenados del arreglo ESTO SE MUESTRA EN CONSOLA
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println(arreglo[i]);
+        }
+
+        //Mostrando los resultados FINALES ordenados del arreglo en un JOption
+        String aux = "Arreglo ordenado: \n";
+        for (int i = 0; i < arreglo.length; i++) {
+            aux = aux + arreglo[i] + "\n";
+        }
+        JOptionPane.showMessageDialog(null, aux);
+    }
+
+    //Ordenando un arreglo de 2 dimensiones de MENOR A MAYOR UTILIZANDO METODO DE LA BURBUJA
+    void arregloDosDimensionesOrdenado(int[][] arregloParaOrdenar) {
+        //Para ordenar un arreglo de dos dimensiones primero pasamos todos sus datos a un arreglo de una dimension para poder asi ordenarlos con el metodo de la burbuja
+
+        //Declarando un array de longitud n*n de acuerdo al original
+        int[] arregloBurbuja = new int[arregloParaOrdenar.length * arregloParaOrdenar[0].length];
+
+        //Asignando los valores en desorden al arreglo de una dimension
+        int posicion = 0;
+        for (int fila = 0; fila < arregloParaOrdenar.length; fila++) {
+            for (int columna = 0; columna < arregloParaOrdenar[0].length; columna++) {
+                arregloBurbuja[posicion] = arregloParaOrdenar[fila][columna];
+                posicion++;
+            }
+        }
+
+        //ORDENANDO CON METODO BURBUJA
+        int variableAuxiliar;
+
+        //Ordenandolos elementos
+        for (int pasadas = 1; pasadas < arregloBurbuja.length; pasadas++) {
+            for (int comparaciones = 1; comparaciones <= arregloBurbuja.length - 1; comparaciones++) {
+
+                if (arregloBurbuja[comparaciones - 1] > arregloBurbuja[comparaciones]) {
+                    variableAuxiliar = arregloBurbuja[comparaciones - 1];
+                    arregloBurbuja[comparaciones - 1] = arregloBurbuja[comparaciones];
+                    arregloBurbuja[comparaciones] = variableAuxiliar;
+                }
+            } //AQUI TERMINA CICLO DE COMPARACIONES      
+        } //AQUI TERMINA CICLO DE PASADAS
+
+        //Una vez asignados, los volvemos a acomodar en el arreglo original (PERO YA ORDENADOS)
+        posicion = 0;
+        for (int fila = 0; fila < arregloParaOrdenar.length; fila++) {
+            for (int columna = 0; columna < arregloParaOrdenar[0].length; columna++) {
+                arregloParaOrdenar[fila][columna] = arregloBurbuja[posicion];
+                posicion++;
+            }
+        }
+
+    }
+
 }
